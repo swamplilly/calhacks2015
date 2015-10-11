05bbfe45f7c88df68a22288d1790689d8dfd9dfc
Lilly An <li.liwei94@gmail.com>
2015-10-11 00:46:38 -0700

    cleaned up alert popups

--- bamfjav.js
+++ bamfjav.js
@@ -18,48 +18,24 @@ window.onload = function()
 	{
 		x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
 		var httpRequest = new XMLHttpRequest();
-		httpRequest.onreadystatechange = function()
-		{
-			if (httpRequest.readyState === 4)
-			{
-				alert("done");
-			}
-		};
+		// httpRequest.onreadystatechange = function()
+		// {
+		// 	if (httpRequest.readyState === 4)
+		// 	{
+		// 		alert("done");
+		// 	}
+		// };
 		httpRequest.open('POST', "http://localhost/api", true);
 		httpRequest.setRequestHeader('Content-Type', 'application/json');
 		var coords = {lat: position.coords.latitude, lon: position.coords.longitude}
 		httpRequest.send(JSON.stringify(coords));
-		alert(JSON.stringify(coords));
+		// alert(JSON.stringify(coords));
 	}
 
 	function error()
 	{
 
 	}
-
-	// function showError(error)
-	// {
- //    	switch(error.code)
- //    	{
- //        	case error.PERMISSION_DENIED:
- //            	x.innerHTML = "User denied the request for Geolocation."
- //            	break;
- //     	   case error.POSITION_UNAVAILABLE:
- //        	    x.innerHTML = "Location information is unavailable."
- //        	    break;
- //      	  case error.TIMEOUT:
- //        	    x.innerHTML = "The request to get user location timed out."
- //            	break;
- //     	   case error.UNKNOWN_ERROR:
- //        	    x.innerHTML = "An unknown error occurred."
- //            	break;
- //    	}
-	// }
-
-	// document.getElementById("#btn").onclick = function()
-	// {
-	// 		getLocation();
-	// }
 
 	document.getElementById("#zipcode").onkeypress = function(e)
 	{
@@ -70,9 +46,5 @@ window.onload = function()
 			getLocation();
 		}
 	}
-	// document.getElementById("#form").onsubmit = function()
-	// {
-	// 		getLocation();
-	// }
 }
 
