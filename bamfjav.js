31feccbb1df916935ed23db242f4800cadc33a9e
Lilly An <li.liwei94@gmail.com>
2015-10-10 19:58:21 -0700

    added stuff to test json, hope this works out

--- bamfjav.js
+++ bamfjav.js
@@ -21,6 +21,23 @@ function error()
 {}
 
 
-
+function AJAX_JSON_Req( url )
+{
+    var AJAX_req = new XMLHttpRequest();
+    AJAX_req.open( "GET", url, true );
+    AJAX_req.setRequestHeader("Content-type", "application/json");
+ 
+    AJAX_req.onreadystatechange = function()
+    {
+        if( AJAX_req.readyState == 4 && AJAX_req.status == 200 )
+        {
+            var response = JSON.parse( AJAX_req.responseText );
+            document.write( response.name );
+        }
+    }
+    AJAX_req.send();
+}
+ 
+AJAX_JSON_Req('coordinates.json');
 
 
